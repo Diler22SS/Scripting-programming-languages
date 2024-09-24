469d38118afac45057297517bb6c4665cd8f75fb
Diler22SS <baintuyev@gmail.com>
2024-09-24 18:44:37 +0800

    tcp done

--- lab 2 tcp udp/tcp/tcp_client.py
+++ lab 2 tcp udp/tcp/tcp_client.py
@@ -8,13 +8,20 @@ import socket
 HOST = '127.0.0.1'
 PORT = 5000
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    print("Connected")
-    while True:
-        print("Sending data")
-        msg = input("you: ")
-        s.sendall(msg.encode())
-        print("Recieving data")
-        data = s.recv(1024)
-        print('Echoing: ', repr(data))
+def start_client(message):
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    client.connect((HOST, PORT))
+    print(f'Client is connected {HOST}:{PORT}')
+    
+    client.sendall(message.encode('utf-8'))
+    
+    data = client.recv(1024)
+    print(f"Recieved message from server: {data.decode('utf-8')}")
+
+    client.close()
+        
+        
+if __name__ == "__main__":
+    message = input('Input message: ')
+    start_client(message)
--- lab 2 tcp udp/tcp/tcp_server.py
+++ lab 2 tcp udp/tcp/tcp_server.py
@@ -2,17 +2,34 @@ import socket
 
 '''
 Задание 1. Реализация TCP-сервера и TCP-клиента
-СЕРВЕР
+КЛИЕНТ
 '''
 
 HOST = '127.0.0.1'
 PORT = 5000
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    conn, addr = s.accept()
-    with conn:
-        while True:
-            data = conn.recv(1024)
-            conn.sendall(data)
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    
+    server.listen(1)
+    print(f'Server is awaiting a connecting to {HOST}:{PORT}')
+    
+    while True:
+        client, client_adr = server.accept()
+        print(f'Client is connected {client_adr}')
+        
+        data = client.recv(1024)
+        if data:
+            print(f'Recieved message from client: {data.decode('utf-8')}')
+
+            client.sendall(data)
+            print('Message was send')
+            
+            server.close()
+            break
+        
+        
+        
+if __name__ == "__main__":
+    start_server()
